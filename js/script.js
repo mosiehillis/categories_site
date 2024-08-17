737226d4213d6e78f90382ba1f53ae052dcc7a28
Steven Hillis <steven.hillis@deepgram.com>
2024-08-17 16:27:28 -0500

    rollback some bad changes

--- js/script.js
+++ js/script.js
@@ -6,30 +6,23 @@ function loadPhotos(category) {
             photos.sort((a, b) => new Date(b.date) - new Date(a.date));  // Sort photos newest to oldest
             photos.forEach(photo => {
                 const img = document.createElement('img');
-                img.src = `photos/${category}/thumbnails/${photo.file}`;
-                img.dataset.fullImage = `photos/${category}/${photo.file}`;
-                img.loading = "lazy";
-                img.onclick = () => img.src = img.dataset.fullImage;  // Load full image on click
+                img.src = `photos/${category}/${photo.file}`;
                 photoGrid.appendChild(img);
             });
         })
         .catch(error => console.error('Error loading photos:', error));
 }
 
-// function setTileBackground(category) {
-//     fetch(`js/${category.toLowerCase()}_photos.json`)
-//         .then(response => response.json())
-//         .then(photos => {
-//             // Filter for portrait-oriented photos
-//             const portraitPhotos = photos.filter(photo => photo.portrait);
-            
-//             if (portraitPhotos.length > 0) {
-//                 const lastPortraitPhoto = portraitPhotos[portraitPhotos.length - 1];
-//                 const tile = document.getElementById(`${category.toLowerCase()}-tile`);
-//                 tile.style.backgroundImage = `url('photos/${category}/${lastPortraitPhoto.file}')`;
-//             }
-//         })
-//         .catch(error => console.error('Error loading category preview:', error));
-// }
+function setTileBackground(category) {
+    fetch(`js/${category.toLowerCase()}_photos.json`)
+        .then(response => response.json())
+        .then(photos => {
+            if (photos.length > 0) {
+                const tile = document.getElementById(`${category.toLowerCase()}-tile`);
+                tile.style.backgroundImage = `url('photos/${category}/${photos[0].file}')`;
+            }
+        })
+        .catch(error => console.error('Error loading category preview:', error));
+}
 
-// ['Concerts', 'Weddings', 'Portraits'].forEach(setTileBackground);
+['Concerts', 'Weddings', 'Portraits'].forEach(setTileBackground);
