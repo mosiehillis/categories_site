2e0ffef59571153e8bf76b493fd9b52b38bbb08b
Steven Hillis <steven.hillis@deepgram.com>
2024-08-17 20:32:14 -0500

    buttons on all pages

--- generate_pages.py
+++ generate_pages.py
@@ -12,37 +12,25 @@ def load_photos(category):
         photo['category'] = category
     return photos
     
-def generate_index_page(categories, recent_photos):
+def generate_html_page(categories, recent_photos, page_name):
     with open('templates/index_template.html', 'r') as f:
         template = f.read()
 
-    category_buttons = ''.join(f'<a href="{category}.html" class="category-button">{category}</a>' for category in categories)
+    category_buttons = '\n'.join(f'<a href="{category}.html" class="category-button">{category}</a>' for category in categories)
     
     photos_html = generate_photos_html(recent_photos)
 
     final_html = template.replace('{{ category_buttons }}', category_buttons)
     final_html = final_html.replace('{{ recent_photos }}', photos_html)
 
-    with open('index.html', 'w') as f:
-        f.write(final_html)
-
-def generate_category_page(category, photos):
-    with open('templates/category_template.html', 'r') as f:
-        template = f.read()
-    
-    photos_html = generate_photos_html(photos)
-
-    final_html = template.replace('{{ category_name }}', category)
-    final_html = final_html.replace('{{ category_photos }}', photos_html)
-
-    with open(f'{category}.html', 'w') as f:
+    with open(f'{page_name}.html', 'w') as f:
         f.write(final_html)
 
 def generate_photos_html(photos):
     photos_html = ''
     for i, photo in enumerate(photos):
         portrait_class = 'portrait' if photo['portrait'] else 'landscape'
-        photos_html += f'<img src="photos/{photo["category"]}/{photo["file"]}" loading="lazy" class="{portrait_class}" alt="Photo" />\n'
+        photos_html += f'<img src="photos/{photo["category"]}/{photo["file"]}" loading="lazy" class="{portrait_class}" alt="Photo"</img>\n'
     return photos_html
 
 def main():
@@ -52,11 +40,11 @@ def main():
     # Sort photos by date descending
     all_photos.sort(key=lambda x: datetime.fromisoformat(x['date']), reverse=True)
 
-    generate_index_page(categories, all_photos)
+    generate_html_page(categories, all_photos, 'index')
 
     for category in categories:
         photos = load_photos(category)
-        generate_category_page(category, photos)
+        generate_html_page(category, photos, category)
 
 if __name__ == '__main__':
     main()
